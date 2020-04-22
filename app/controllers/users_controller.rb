1b883a4f3f9a2e8903fcbd42ff9e7990d8798d85
kwizl <karis.njoroge@gmail.com>
2020-04-22 18:01:59 +0300

    Implement code review corrections

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,17 @@
 class UsersController < ApplicationController
-  include UsersHelper
-
-  def new; end
+  def new
+    @user = User.new
+  end
 
   def create
     @user = User.new(user_params)
 
-    if @user.save
-      redirect_to new_user_path
-    else
-      render :new
+    respond_to do |format|
+      if @user.save
+        format.html { redirect_to new_user_path }
+      else
+        format.html { render :new }
+      end
     end
   end
 
@@ -25,5 +27,11 @@ class UsersController < ApplicationController
     else
       render :edit
     end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
   end
 end
